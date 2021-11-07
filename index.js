3a3d24dd434bc33427d93e98ca3d362ee9afeda6
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-07 15:06:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 const pokemonRouter = require("./src/routers/pokemonRouter");
 const userRouter = require("./src/routers/userRouter");
 const errorHandler = require("./src/middleware/errorHandler");
 const userHandler = require("./src/middleware/userHandler");
 
 app.use(cors());
+app.use("/", express.static(path.resolve(`./dist`)));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./dist/index.html"));
+});
 app.use(express.json());
 app.use(userHandler);
 app.use("/pokemon", pokemonRouter);
