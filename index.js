6b478b2098147767c9b5f17610b79524a3a1400f
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-07 15:01:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 8080;
-const pokemonRouter = require('./src/routers/pokemonRouter');
-const userRouter = require('./src/routers/userRouter');
-const errorHandler = require('./src/middleware/errorHandler');
-const userHandler = require('./src/middleware/userHandler');
+const port = process.env.PORT || 3000;
+const pokemonRouter = require("./src/routers/pokemonRouter");
+const userRouter = require("./src/routers/userRouter");
+const errorHandler = require("./src/middleware/errorHandler");
+const userHandler = require("./src/middleware/userHandler");
 
 app.use(cors());
 app.use(express.json());
 app.use(userHandler);
-app.use('/pokemon', pokemonRouter);
-app.use('/info', userRouter);
+app.use("/pokemon", pokemonRouter);
+app.use("/info", userRouter);
 app.use(errorHandler);
 
 // start the server
 app.listen(port, function () {
-	console.log('app started');
+  console.log("app started");
 });
